2dc757ef2698ae4fb345b044b5ca84dbdde4ba0d
yahir pedraza <yahirpedraza-26@hotmail.com>
2024-06-12 00:58:29 -0600

    LOGIN FUNCIONANDO

--- AR/php/login_usua.php
+++ AR/php/login_usua.php
@@ -9,7 +9,22 @@ session_start();
     $validar_log = mysqli_query($conexion, "SELECT * FROM login_medic WHERE Correo='Correo' and Contraseña='$Contraseña'");
     $_SESSION['login_medic'] = $Correo;
     $filas=mysqli_fetch_array($validar_log);
-
-
+  //ADMIN
+      /*  $_SESSION['id']=$filas;*/
+      if($filas['id']==6){
+        header("Location: ../AR/html/admin.php");
+       exit;
+   }else if (mysqli_num_rows($validar_log) > 0 ){
+       header("Location:  ../AR/html/inde.php");
+       exit;
+   }else{
+       echo' 
+       <script>
+       alert("Por favor registrate o inicia sesión");
+       window.location = "../AR/html/index.php";
+       </script>
+       ';
+       exit;
+   }
 
 ?>
