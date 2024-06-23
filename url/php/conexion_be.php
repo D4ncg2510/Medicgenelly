d6a77ba4913fb2ee93897a9c9bb5287239db2ee5
D4ncg2510 <108159079+D4ncg2510@users.noreply.github.com>
2024-06-23 00:57:10 -0600

    Delete url directory

--- url/php/conexion_be.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $conexion = mysqli_connect("localhost","root","","medicgenely");
-    if($conexion){
-        echo 'Conectado exitosamente a la Base dee Datos';
-        }else{
-            echo 'No se ha podido conectar a la base de datos';
-        }
-
-?>
