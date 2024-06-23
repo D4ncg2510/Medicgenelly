fda82cb369580c51e44f20933644d4177d123142
D4ncg2510 <108159079+D4ncg2510@users.noreply.github.com>
2024-06-23 00:32:08 -0600

    Delete AR directory

--- AR/php/cerrar_se.php
+++
@@ -1,8 +0,0 @@
-<?php
- include 'conexion_be.php'; 
-
-             session_start();
-             session_destroy();
-             header('Location:  ../AR/html/index.html');
-          
-?>
--- AR/php/conexion_be.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $conexion = mysqli_connect("localhost","root","","medicgenely");
-    if($conexion){
-        echo 'Conectado exitosamente a la Base de Datos';
-        }else{
-            echo 'No se ha podido conectar a la base de datos';
-        }
-
-?>
